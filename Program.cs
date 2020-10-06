a4d2db3c49ed7a9502bdb2435e21214079e52da3
calebjooste <69714925+calebjooste@users.noreply.github.com>
2020-10-06 15:07:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = 1020;
+
+            Console.WriteLine(budget / 0);
+
+            
+        }
+    }
+}
